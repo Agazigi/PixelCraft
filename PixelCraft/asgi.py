500c1f67f0db8a31bf953d5a226a6a0fdbd78fb5
Agazigi <3486446493@qq.com>
2025-09-24 23:48:53 +0800

    init project

---
+++ PixelCraft/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for PixelCraft project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PixelCraft.settings')
+
+application = get_asgi_application()
